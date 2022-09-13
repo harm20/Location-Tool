6da0722541eeabed00d0cf477ca61a8bfae1bc55
harm20 <70063520+harm20@users.noreply.github.com>
2022-09-13 21:38:56 +0300

    Update Location.py

--- Location.py
+++ Location.py
@@ -1,7 +1,7 @@
 from geoip import geolite2 
 print("WELCOME HARM TOOL")
 ip = input("Enter The IP : " )
-locator = geolite2.loockup(ip)
+locator = geolite2.lookup(ip)
 if locator is None :
   print("UNKNOWN")
 else :
